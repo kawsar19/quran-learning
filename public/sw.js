ea05ac79fa5c6631def3f2c37b6142005e31c68e
kawsar19 <kawsarbinjahangir@gmail.com>
2026-01-31 10:40:10 +0600

    update sw.js

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,7 @@ const STATIC_ASSETS = [
   '/manifest.webmanifest',
 ];
 
-// Install event - cache static assets
+// Install  event - cache static assets
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
